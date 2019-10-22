3b3bcffef8a4635f6126e172dee626a03c589015
Gökhan Şişman <pa1ngokhan@gmail.com>
2019-10-22 10:56:15 +0300

    Urun Page 2

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ var foto = new Vue({
     seen: true
   }
 })
+var tabler = new Vue({
+  el: '#tabler',
+  data: { activetab: 1 },
+});
+new Vue({
+  el: '#app',
+  data: { 
+    counter: 0 // Initial Value 
+  },
+  methods: {
+    increaseCounter(increaseLimit) { // Increase
+      if (this.counter < increaseLimit) this.counter++;
+    },
+    decreaseCounter(decreaseLimit) { // Decrease
+      if (this.counter > decreaseLimit) this.counter--;
+    }, 
+    resetCounter() { // Reset
+      this.counter = 0;
+    }
+  },
+  computed: {
+    output() { // Output for computed property!
+      return this.counter >= 10 ? '10 or more' : 'Less than 10';
+    }
+  }
+});
 
 
 var slideIndex = 1;
